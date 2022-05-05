e70b82aab917e65c27d309e539bcd2bb6feba9b3
Allen Porter <allen.porter@gmail.com>
2022-05-04 21:31:41 -0700

    Add ability to reset local store on code incompatibility changes (#45)
    Fixes #36

--- tests/test_event_sync.py
+++ tests/test_event_sync.py
@@ -2,13 +2,14 @@
 
 import datetime
 from collections.abc import Awaitable, Callable
+from unittest.mock import patch
 
 import aiohttp
 import pytest
 from freezegun import freeze_time
 
 from gcal_sync.api import GoogleCalendarService, LocalListEventsRequest
-from gcal_sync.event_sync import CalendarEventSyncManager
+from gcal_sync.event_sync import VERSION, CalendarEventSyncManager
 from gcal_sync.exceptions import ApiException
 from gcal_sync.model import DateOrDatetime, Event
 from gcal_sync.store import CalendarStore, InMemoryCalendarStore
@@ -461,3 +462,78 @@ async def test_invalidated_sync_token(
             end=DateOrDatetime(date=datetime.date(2022, 4, 13)),
         ),
     ]
+
+
+@freeze_time("2022-04-05 07:31:02", tz_offset=-7)
+async def test_token_version_invalidation(
+    event_sync_manager_cb: Callable[[], Awaitable[CalendarEventSyncManager]],
+    json_response: ApiResult,
+    url_request: Callable[[], str],
+    request_reset: Callable[[], str],
+) -> None:
+    """Test lookup events API."""
+
+    json_response(
+        {
+            "items": [
+                {
+                    "id": "some-event-id-1",
+                    "summary": "Event 1",
+                    "description": "Event description 1",
+                    "start": {
+                        "date": "2022-04-13",
+                    },
+                    "end": {
+                        "date": "2022-04-14",
+                    },
+                    "status": "confirmed",
+                    "transparency": "transparent",
+                },
+            ],
+            "nextSyncToken": "sync-token-1",
+        }
+    )
+    sync = await event_sync_manager_cb()
+    await sync.run()
+    assert url_request() == [
+        "/calendars/some-calendar-id/events?maxResult=100&singleEvents=true&orderBy=startTime"
+        "&fields=kind,nextPageToken,nextSyncToken,items(id,summary,description,location,start"
+        ",end,transparency,timeZone)&timeMin=2022-03-08T00:31:02"
+    ]
+
+    result = await sync.store_service.async_list_events(LocalListEventsRequest())
+    assert len(result.events) == 1
+    assert result.events[0].id == "some-event-id-1"
+
+    request_reset()
+    json_response(
+        {
+            "items": [
+                {
+                    "id": "some-event-id-2",
+                    "summary": "Event 2",
+                    "description": "Event description 2",
+                    "start": {
+                        "date": "2022-04-15",
+                    },
+                    "end": {
+                        "date": "2022-04-20",
+                    },
+                    "transparency": "opaque",
+                },
+            ],
+            "nextSyncToken": "sync-token-2",
+        }
+    )
+
+    with patch("gcal_sync.event_sync.VERSION", VERSION + 1):
+        await sync.run()
+
+    assert url_request() == [
+        "/calendars/some-calendar-id/events?maxResult=100&singleEvents=true&orderBy=startTime"
+        "&fields=kind,nextPageToken,nextSyncToken,items(id,summary,description,location,start"
+        ",end,transparency,timeZone)&timeMin=2022-03-08T00:31:02"
+    ]
+    result = await sync.store_service.async_list_events(LocalListEventsRequest())
+    assert len(result.events) == 1
+    assert result.events[0].id == "some-event-id-2"
